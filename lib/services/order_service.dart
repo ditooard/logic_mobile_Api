05a794c7819ba3e4dd9ae38f7d3ccd05b4306629
Dito Ardi Pratama <127459191+ditooard@users.noreply.github.com>
2023-09-25 18:11:35 +0700

    update

--- lib/services/order_service.dart
+++ lib/services/order_service.dart
@@ -1,29 +1,26 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class OrderService {
-  final String baseUrl = 'http://dev.komship.komerce.my.id/api/v2/admin/order';
+  final String baseUrl = 'https://dev.komship.komerce.my.id/api/v2/admin/order';
 
   Future<Map<String, dynamic>> getOrderDetails(String orderNumber) async {
     final response = await http.get(
-      Uri.parse(
-          'http://dev.komship.komerce.my.id/api/v2/admin/order/search?search=KOM23091178316541'),
-      // Send authorization headers to the backend.
+      Uri.parse('$baseUrl/search?search=$orderNumber'), // Menggunakan variabel orderNumber
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': 'GATEWAYKOMSHIPKOMERCE'
       },
     );
-    final responseJson = jsonDecode(response.body);
 
     print('cekkkk ${response.body}');
 
     if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      return data;
+      return jsonDecode(response.body);
     } else {
-      throw Exception('Gagal mengambil detail pesanan');
+      // Menggunakan pesan kesalahan dari respons HTTP jika tersedia
+      final errorMessage = jsonDecode(response.body)['message'] ?? 'Gagal mengambil detail pesanan';
+      throw Exception(errorMessage);
     }
   }
 }
